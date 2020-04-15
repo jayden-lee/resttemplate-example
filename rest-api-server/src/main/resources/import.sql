a6baeb867b15ac0cf474c6c3403f883ce407b4ad
이광기 <gwanggi.lee@gmail.com>
2020-04-15 14:24:09 +0900

    Remove old module

--- rest-api-server/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into user (email, name) values ('kelly@abc.com', 'kelly');
-insert into user (email, name) values ('james@abc.com', 'james');
-insert into user (email, name) values ('ella@abc.com', 'ella');
-insert into user (email, name) values ('andrew@abc.com', 'andrew');
-insert into user (email, name) values ('robb@abc.com', 'robb');
-insert into user (email, name) values ('david@abc.com', 'david');
