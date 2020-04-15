2a22c8564dc79332e57612352c651cd95c192ddd
이광기 <gwanggi.lee@gmail.com>
2020-04-15 15:28:46 +0900

    Modify logging request interceptor

--- client/src/main/kotlin/com/jayden/client/config/LoggingRequestInterceptor.kt
+++ client/src/main/kotlin/com/jayden/client/config/LoggingRequestInterceptor.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpRequest
 import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
+import org.springframework.util.StopWatch
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.nio.charset.Charset
@@ -15,29 +16,36 @@ class LoggingRequestInterceptor : ClientHttpRequestInterceptor {
 
     override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
         traceRequest(request, body)
+
+        val stopWatch = StopWatch()
+        stopWatch.start()
         val response = execution.execute(request, body)
-        traceResponse(response)
+        stopWatch.stop()
+
+        traceResponse(response, stopWatch)
+
         return response
     }
 
     private fun traceRequest(request: HttpRequest, body: ByteArray) {
-        log.info("===========================[REQUEST]====================================");
+        log.info("===========================[REQUEST]====================================")
         log.info("URI         : {}", request.uri)
         log.info("Method      : {}", request.method)
         log.info("Headers     : {}", request.headers)
         log.info("Request body: {}", String(body, Charset.forName("UTF-8")))
     }
 
-    private fun traceResponse(response: ClientHttpResponse) {
+    private fun traceResponse(response: ClientHttpResponse, stopWatch: StopWatch) {
         val inputStringBuilder = StringBuilder()
-        val bufferedReader = BufferedReader(InputStreamReader(response.body, "UTF-8"));
+        val bufferedReader = BufferedReader(InputStreamReader(response.body, "UTF-8"))
         var line = bufferedReader.readLine()
         while (line != null) {
             inputStringBuilder.append(line)
             inputStringBuilder.append('\n')
             line = bufferedReader.readLine()
         }
-        log.info("===========================[RESPONSE]====================================");
+        log.info("===========================[RESPONSE]====================================")
+        log.info("Response time: {}s", stopWatch.totalTimeSeconds)
         log.info("Status code  : {}", response.statusCode)
         log.info("Status text  : {}", response.statusText)
         log.info("Headers      : {}", response.headers)
